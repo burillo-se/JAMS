0ff788e4c470455a029cd1676eb536a6c1b6cdb3
burillo-se <spam@trap.com>
2016-09-25 11:07:01 +0100

    Add grid-based locality code from BARABAS

--- JAMS.cs
+++ JAMS.cs
@@ -7,6 +7,146 @@
  *
  */
 
+public List<JAMS_Group> active_airlocks = new List<JAMS_Group>();
+public List<JAMS_Group> airlocks = new List<JAMS_Group>();
+public List<IMyCubeGrid> local_grids = new List<IMyCubeGrid>();
+
+// state machine
+Action [] states = null;
+
+int current_state;
+
+/*
+ * Graph-based grid locality code transplanted from BARABAS.
+ */
+
+// grid graph edge class, represents a connection point between two grids.
+public class Edge < T > {
+ public T src {get; set;}
+ public T dst {get; set;}
+}
+
+// comparer for graph edges - the way the comparison is done means the edges are
+// bidirectional - meaning, it doesn't matter which grid is source and which
+// grid is destination, they will be equal as far as comparison is concerned.
+public class EdgeComparer < T > : IEqualityComparer < Edge < T > > {
+ public int GetHashCode(Edge < T > e) {
+  // multiply src hashcode by dst hashcode - multiplication is commutative, so
+  // result will be the same no matter which grid was source or destination
+  return e.src.GetHashCode() * e.dst.GetHashCode();
+ }
+ public bool Equals(Edge < T > e1, Edge < T > e2) {
+  if (e1.src.Equals(e2.src) && e1.dst.Equals(e2.dst)) {
+   return true;
+  }
+  if (e1.src.Equals(e2.dst) && e1.dst.Equals(e2.src)) {
+   return true;
+  }
+  return false;
+ }
+}
+
+// our grid graph
+public class Graph < T > {
+ public Graph() {
+  cmp = new EdgeComparer < T >();
+  v_edges = new Dictionary < T, HashSet < Edge < T > > >();
+  r_edges = new HashSet < Edge < T > >(cmp);
+ }
+
+ // add an edge to the graph
+ public void addEdge(T src, T dst, bool is_remote) {
+  var t = new Edge<T>();
+  t.src = src;
+  t.dst = dst;
+
+  // remote edges don't need to be added to local list of edges
+  if (is_remote) {
+   r_edges.Add(t);
+   return;
+  }
+
+  // add edge to list of per-vertex edges
+  HashSet < Edge < T > > hs_src, hs_dst;
+  if (!v_edges.TryGetValue(src, out hs_src)) {
+   hs_src = new HashSet < Edge < T > >(cmp);
+   v_edges.Add(src, hs_src);
+  }
+  if (!v_edges.TryGetValue(dst, out hs_dst)) {
+   hs_dst = new HashSet < Edge < T > >(cmp);
+   v_edges.Add(dst, hs_dst);
+  }
+  hs_src.Add(t);
+  hs_dst.Add(t);
+ }
+
+ // get all grids that are local to source grid (i.e. all grids connected by
+ // rotors or pistons)
+ public List < T > getGridRegion(T src) {
+  // if there never was a local edge from/to this grid, it's by definition
+  // the only grid in this region
+  if (!v_edges.ContainsKey(src)) {
+   return new List < T >() {src};
+  }
+  // otherwise, gather all vertices in this region
+  var region = new List<T>();
+  var seen = new HashSet<T>();
+  var next = new Queue<T>();
+  next.Enqueue(src);
+  while (next.Count != 0) {
+   var g = next.Dequeue();
+   if (!seen.Contains(g)) {
+    var edges = v_edges[g];
+    foreach (var edge in edges) {
+     next.Enqueue(edge.src);
+     next.Enqueue(edge.dst);
+    }
+    seen.Add(g);
+    region.Add(g);
+   }
+  }
+  return region;
+ }
+
+ // this must be called after adding all edges. what this does is, it removes
+ // edges that aren't supposed to be there. For example, if you have grids
+ // A, B, C, local edges A->B and B->C, and a remote edge C->A, there is a path
+ // from C to A through local edges, so the remote edge should not count as an
+ // actual "remote" edge, and therefore should be removed.
+ public void validateGraph() {
+  var to_remove = new HashSet < Edge <T> >(cmp);
+  var seen = new HashSet<T>();
+  foreach (var edge in r_edges) {
+   var next = new Queue<T>();
+   next.Enqueue(edge.src);
+   next.Enqueue(edge.dst);
+   while (next.Count != 0) {
+    var g = next.Dequeue();
+    if (!seen.Contains(g)) {
+     var region = new HashSet<T>(getGridRegion(g));
+     seen.UnionWith(region);
+     // find any edges that are completely inside this region, and remove them
+     foreach (var e in r_edges) {
+      if (region.Contains(e.src) && region.Contains(e.dst)) {
+       to_remove.Add(e);
+      }
+     }
+    }
+   }
+  }
+  foreach (var e in to_remove) {
+   r_edges.Remove(e);
+  }
+ }
+
+ // our comparer to use with all sets
+ EdgeComparer < T > cmp;
+ // list of all edges
+ HashSet < Edge < T > > r_edges;
+ // dictionaries of edges for each vertex
+ Dictionary < T, HashSet < Edge < T > > > v_edges;
+}
+
 public abstract class JAMS_Group {
  public abstract void updateFromGroup(IMyBlockGroup g);
  public abstract void updateFromString(string s);
@@ -470,13 +610,147 @@ JAMS_Group createFromGroup(IMyBlockGroup g) {
  return null;
 }
 
-public List<JAMS_Group> active_airlocks = new List<JAMS_Group>();
-public List<JAMS_Group> airlocks = new List<JAMS_Group>();
+/*
+ * Grid stuff
+ */
 
-// state machine
-Action [] states = null;
 
-int current_state;
+// find which grid has a block at world_pos, excluding "self"
+IMyCubeGrid findGrid(Vector3D w_p, IMyCubeGrid self, List < IMyCubeGrid > grids) {
+ foreach (var g in grids) {
+  if (g == self) {
+   continue;
+  }
+  var pos = g.WorldToGridInteger(w_p);
+  if (g.CubeExists(pos)) {
+   return g;
+  }
+ }
+ return null;
+}
+
+IMyCubeGrid getConnectedGrid(IMyShipConnector c) {
+ if (!c.IsConnected) {
+  return null;
+ }
+ // skip connectors connecting to the same grid
+ var o = c.OtherConnector;
+ if (o.CubeGrid == c.CubeGrid) {
+  return null;
+ }
+ return o.CubeGrid;
+}
+
+IMyCubeGrid getConnectedGrid(IMyMotorBase r, List < IMyCubeGrid > grids) {
+ if (!r.IsAttached) {
+  return null;
+ }
+ var pos = r.Position;
+ var or = r.Orientation;
+ var dir = new Vector3I(0, 1, 0);
+ Matrix m;
+ or.GetMatrix(out m);
+ Vector3I.Transform(ref dir, ref m, out dir);
+ var w_p = r.CubeGrid.GridIntegerToWorld(pos + dir);
+ return findGrid(w_p, r.CubeGrid, grids);
+}
+
+IMyCubeGrid getConnectedGrid(IMyPistonBase p, List < IMyCubeGrid > grids) {
+ if (!p.IsAttached) {
+  return null;
+ }
+ var pos = p.Position;
+ var or = p.Orientation;
+ bool is_large = p.BlockDefinition.ToString().Contains("Large");
+ var up = (int) Math.Round(p.CurrentPosition / (is_large ? 2.5 : 0.5));
+ var dir = new Vector3I(0, 2 + up, 0);
+ Matrix m;
+ or.GetMatrix(out m);
+ Vector3I.Transform(ref dir, ref m, out dir);
+ var w_p = p.CubeGrid.GridIntegerToWorld(pos + dir);
+ return findGrid(w_p, p.CubeGrid, grids);
+}
+
+// getting local grids is not trivial, we're basically building a graph of all
+// grids and figure out which ones are local to us. we are also populating
+// object lists in the meantime
+List < IMyCubeGrid > getLocalGrids(bool force_update = false) {
+ if (local_grids != null && !force_update) {
+  return local_grids;
+ }
+
+ // piston and rotor lists are local, we don't need them once we're done
+ var blocks = new List<IMyTerminalBlock>();
+ var pistons = new List < IMyTerminalBlock > ();
+ var rotors = new List < IMyTerminalBlock > ();
+ var connectors = new List < IMyTerminalBlock > ();
+ var grids_set = new HashSet<IMyCubeGrid>();
+
+ // get all blocks that are accessible to GTS
+ GridTerminalSystem.GetBlocks(blocks);
+
+ // for each block, get its grid, store data for this grid, and populate respective
+ // object list if it's one of the objects we're interested in
+ foreach (var b in blocks) {
+  if (slimBlock(b) == null) {
+   continue;
+  }
+  grids_set.Add(b.CubeGrid);
+
+  // fill all lists
+  if (b is IMyShipConnector) {
+   connectors.Add(b);
+  } else if (b is IMyPistonBase) {
+   pistons.Add(b);
+  } else if (b is IMyMotorBase) {
+   rotors.Add(b);
+  }
+ }
+ // free the memory!
+ blocks.Clear();
+ var grids = new List<IMyCubeGrid>(grids_set);
+
+ // now, build a graph of all grids
+ var gr = new Graph<IMyCubeGrid>();
+
+ // first, go through all pistons
+ foreach (IMyPistonBase p in pistons) {
+  var connected_grid = getConnectedGrid(p, grids);
+
+  if (connected_grid != null) {
+   // grids connected to pistons are local to their source
+   gr.addEdge(p.CubeGrid, connected_grid, false);
+  }
+ }
+
+ // do the same for rotors
+ foreach (IMyMotorBase rotor in rotors) {
+  var connected_grid = getConnectedGrid(rotor, grids);
+
+  if (connected_grid != null) {
+   // grids connected to locals are local to their source
+   gr.addEdge(rotor.CubeGrid, connected_grid, false);
+  }
+ }
+
+ // do the same for connectors
+ foreach (IMyShipConnector c in connectors) {
+  var connected_grid = getConnectedGrid(c);
+
+  if (connected_grid != null) {
+   // grids connected to connectors belong to a different ship
+   gr.addEdge(c.CubeGrid, connected_grid, true);
+  }
+ }
+
+ // make sure we remove all unnecessary edges from the graph
+ gr.validateGraph();
+
+ // now, get our actual local grid
+ local_grids = gr.getGridRegion(Me.CubeGrid);
+
+ return local_grids;
+}
 
 void showOnHud(IMyTerminalBlock b) {
  if (b.GetProperty("ShowOnHUD") != null) {
@@ -491,10 +765,11 @@ void hideFromHud(IMyTerminalBlock b) {
 }
 
 public void filterLocalGrid(List < IMyTerminalBlock > blocks) {
+ var grids = getLocalGrids();
  for (int i = blocks.Count - 1; i >= 0; i--) {
   var block = blocks[i];
   var grid = block.CubeGrid;
-  if (grid != Me.CubeGrid) {
+  if (!grids.Contains(grid)) {
    blocks.RemoveAt(i);
   }
  }
@@ -657,6 +932,10 @@ void s_processAirlocks() {
  }
 }
 
+void s_refreshGrids() {
+ getLocalGrids(true);
+}
+
 int[] state_cycle_counts;
 int cycle_count;
 
@@ -707,15 +986,21 @@ public void Save() {
 
 public Program() {
  states = new Action [] {
+  s_refreshGrids,
   s_refreshAirlocks,
   s_checkAirlocks,
   s_processAirlocks,
  };
- current_state = 1; // skip initial refresh
  Me.SetCustomName("JAMS CPU");
  hideFromHud(Me);
+
+ // do an out of cycle refresh, because we need to find airlocks that saved to Storage
+ s_refreshGrids();
  s_refreshAirlocks();
  updateAirlocks();
+
+ // make sure we start from state 2, as first two states we already completed
+ current_state = 2;
  state_cycle_counts = new int[states.Length];
 }
 
