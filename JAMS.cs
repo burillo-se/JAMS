272284b0078ca7fe5ee32ec98c6f281f9b05a82e
burillo-se <spam@trap.com>
2019-04-21 15:22:50 +0100

    Remove unneeded imports

--- JAMS.cs
+++ JAMS.cs
@@ -3,19 +3,12 @@
 using System;
 using System.Linq;
 using System.Text;
-using System.Collections;
 using System.Collections.Generic;
 
 using VRageMath;
-using VRage.Game;
-using VRage.Collections;
 using Sandbox.ModAPI.Ingame;
-using VRage.Game.Components;
 using VRage.Game.ModAPI.Ingame;
-using Sandbox.ModAPI.Interfaces;
-using Sandbox.Game.EntityComponents;
 using SpaceEngineers.Game.ModAPI.Ingame;
-using VRage.Game.ObjectBuilders.Definitions;
 
 namespace SpaceEngineers
 {
